2f845e87f379d84eb5e4d101c1c123493f7fd660
Kevin Boos <kevinaboos@gmail.com>
2024-01-27 14:53:41 -0600

    undo height change to `StatusLabel`

--- src/home/rooms_list.rs
+++ src/home/rooms_list.rs
@@ -64,7 +64,7 @@ live_design! {
     Empty = <View> { }
 
     StatusLabel = <View> {
-        width: Fill, height: Fit,
+        width: Fill, height: 80.0,
         align: { x: 0.5, y: 0.5 }
         draw_bg: {
             color: #f4f4f4
