a92baa5d00ba04501b61250f841f40841ba65b5f
Julian Montes de Oca <julian@wyeworks.com>
2024-08-20 17:36:23 +0800

    Minimize font sizes

--- src/shared/styles.rs
+++ src/shared/styles.rs
@@ -2,17 +2,17 @@ use makepad_widgets::*;
 
 live_design! {
     TITLE_TEXT = {
-        font_size: (14),
-        font: {path: dep("crate://makepad-widgets/resources/GoNotoKurrent-Regular.ttf")}
-    }
-
-    REGULAR_TEXT = {
         font_size: (12),
         font: {path: dep("crate://makepad-widgets/resources/GoNotoKurrent-Regular.ttf")}
     }
 
-    TEXT_SUB = {
+    REGULAR_TEXT = {
         font_size: (10),
+        font: {path: dep("crate://makepad-widgets/resources/GoNotoKurrent-Regular.ttf")}
+    }
+
+    TEXT_SUB = {
+        font_size: (9),
         height_factor: 1.5,
         font: {path: dep("crate://makepad-widgets/resources/GoNotoKurrent-Regular.ttf")}
     }
