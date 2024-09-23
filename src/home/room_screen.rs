d01756bf3dcf23004173fa8505807186427185f0
Kevin Boos <kevinaboos@gmail.com>
2024-09-23 21:12:42 +0200

    Ensure the `typing_notice` view state is reset when changing rooms

--- src/home/room_screen.rs
+++ src/home/room_screen.rs
@@ -1571,6 +1571,8 @@ impl RoomScreen {
             (new_tl_state, true)
         };
 
+        // Subscribe to typing notices, but hide the typing notice view initially.
+        self.view(id!(typing_notice)).set_visible(false);
         submit_async_request(
             MatrixRequest::SubscribeToTypingNotices {
                 room_id: room_id.clone(),
@@ -1679,8 +1681,6 @@ impl RoomScreen {
         } else {
             self.clear_replying_to();
         }
-
-        // TODO: FIXME: we need to actually re-draw the replying_preview view here.
     }
 
     /// Sets this `RoomScreen` widget to display the timeline for the given room.
