b97ab00b5669a7430dad891ee8de9662c449a9c6
aaravlu <demolemon@outlook.at>
2025-01-30 22:59:20 +0800

    No change to 'shared/mod.rs'

--- src/shared/mod.rs
+++ src/shared/mod.rs
@@ -12,7 +12,6 @@ pub mod text_or_image;
 pub mod typing_animation;
 pub mod popup_list;
 pub mod verification_badge;
-// pub mod image_viewer;
 
 pub fn live_design(cx: &mut Cx) {
     // Order matters here, as some widget definitions depend on others.
@@ -28,5 +27,4 @@ pub fn live_design(cx: &mut Cx) {
     popup_list::live_design(cx);
     verification_badge::live_design(cx);
     color_tooltip::live_design(cx);
-    // image_viewer::live_design(cx);
 }
