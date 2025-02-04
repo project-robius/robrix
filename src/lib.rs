901597a12ac11ed88abefbc5fe367b8ab75c10e3
aaravlu <demolemon@outlook.at>
2025-02-04 22:20:21 +0800

    Add doc

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@ pub mod home;
 mod profile;
 /// A modal/dialog popup for interactive verification of users/devices.
 mod verification_modal;
+// A image viewer for viewing images.
 mod image_viewer;
 /// Shared UI components.
 pub mod shared;
