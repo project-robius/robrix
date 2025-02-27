a4d8aa772749c554a633bd301780ececad3e2d86
Cam <yangcancai0112@gmail.com>
2025-02-27 11:00:16 +0800

    hide console window on Windows in release

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // this stub is necessary because some platforms require building
 // as dll (mobile / wasm) and some require to be built as executable
 // unfortunately cargo doesn't facilitate this without a main.rs stub
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 fn main() {
     robrix::app::app_main()
 }
