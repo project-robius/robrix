288b1a15acf9aa68e4cfca65b376ff5f46823e9c
Guocork <g1024536444@gmail.com>
2025-04-14 15:45:26 +0800

    complete

--- src/shared/popup_notification.rs
+++ src/shared/popup_notification.rs
@@ -7,8 +7,8 @@ live_design! {
     use link::shaders::*;
     use link::widgets::*;
 
-    use crate::shared::styles::*;
-    use crate::shared::icon_button::RobrixIconButton;
+    ICO_CLOSE = dep("crate://self/resources/icons/close.svg")
+    ICO_CHECK = dep("crate://self/resources/icons/checkmark.svg")
 
     Progress = <View> {
         width: 20,
@@ -19,7 +19,7 @@ live_design! {
             width: Fill,
             height: Fill,
             draw_bg: {
-                color: #639b0d,
+                color: #42660a,
                 radius: 4.0,
             }
         }
@@ -28,127 +28,251 @@ live_design! {
             height: Fill,
             width: Fill,
             draw_bg: {
-                color: #42660a,
+                color: #639b0d,   // #42660a
                 radius: 4.0,
             }
         }
     }
 
-    PopupDialog = <RoundedView> {
-        width: 275,
+    TipContent = <View> {
+        width: Fill,
         height: Fill,
+        spacing: 15.0,
+        flow: Right,
+        align: {
+            x: 0.0,
+            y: 0.5,
+        }
+        margin: { left: 20.0 }
 
-        <Progress> {}
-    }
+        <Icon> {
+            draw_icon: {
+                svg_file: (ICO_CHECK),
+                color: #42660a,
+            }
+            icon_walk: { width: 18, height: 18 }
+        }
 
+        <Label> {
+            draw_text: {
+                color: #42660a,
+                text_style: {
+                    font_size: 12
+                }
+            }
+            text: "Successfully updated transaction",
+        }
 
-    pub RobrixPopupNotification = {{RobrixPopupNotification}} {
-        draw_bg: {
-            fn pixel(self) -> vec4 {
-                return vec4(193,255,193,1.0);
+        close_icon = <View> {
+            width: Fit,
+            height: Fit,
+            cursor: Hand,
+            <Icon> {
+                draw_icon: {
+                    svg_file: (ICO_CLOSE),
+                    color: #6cc328
+                }
+    
+                icon_walk: { width: 16, height: 16 }
             }
         }
-        content: {
-            <PopupDialog> {}
-        }
         
+    }
+
+    PopupDialog = <RoundedView> {
+        width: 375,
+        height: 100,
+        flow: Right,
+
+        show_bg: true,
+        draw_bg: {
+            color: #d3f297,
+        }
+
+        <Progress> {}
+        <TipContent> {}
+    }
+
+    pub RobrixPopupNotification = {{RobrixPopupNotification}} {
+        width: Fit
+        height: Fit
+        flow: Overlay
+        abs_pos: vec2(10.0, 10.0)
+        duration: 2.0
+
+        draw_bg: {
+            fn pixel(self) -> vec4 {
+                return vec4(0., 0., 0., 0.0)
+            }
+        }
+
+        content: <PopupDialog> {}
+
         animator: {
             mode = {
                 default: close,
                 open = {
                     redraw: true,
-                    from: {}
-                    ease: 
-                    apply: {}
+                    from: {all: Forward {duration: 2.0}}
+                    ease: OutQuad
+                    apply: {
+                        abs_pos: vec2(60.0, 10.0),
+                    }
+                }
+                close = {
+                    redraw: true,
+                    from: {all: Forward {duration: 1.0}}
+                    ease: InQuad
+                    apply: {
+                        abs_pos: vec2(-1000.0, 10.0),
+                    }
                 }
             }
         }
     }
 }
 
-#[derive(Live, LiveHook, Widget)]
+#[derive(Live, Widget)]
 pub struct RobrixPopupNotification {
-    #[deref]
-    view: PopupNotification,
+    #[live]
+    #[find]
+    content: View,
 
     #[live]
     duration: f64,
 
-    #[rust]
-    timer: Timer,
+    #[rust(DrawList2d::new(cx))]
+    draw_list: DrawList2d,
 
-    #[rust]
-    start_time: Option<Instant>,
+    #[redraw]
+    #[live]
+    draw_bg: DrawQuad,
+    #[layout]
+    layout: Layout,
+    #[walk]
+    walk: Walk,
 
     #[rust]
     opened: bool,
 
+    #[rust]
+    animation_timer: Timer,
+
+    #[rust]
+    duration_timer: Timer,
+
+    #[rust]
+    start_time: Option<Instant>,
+
     #[animator]
     animator: Animator,
+
+    #[rust]
+    redraw_timer: Timer,
+}
+
+impl LiveHook for RobrixPopupNotification {
+    fn after_apply(&mut self, cx: &mut Cx, _apply: &mut Apply, _index: usize, _nodes: &[LiveNode]) {
+        self.draw_list.redraw(cx);
+    }
 }
 
 impl Widget for RobrixPopupNotification {
     fn handle_event(&mut self, cx: &mut Cx, event: &Event, scope: &mut Scope) {
+        if !self.opened {
+            return;
+        }
 
-        // loop alaways trigger 
-        if self.opened {
-            // self.timer = cx.start_timeout(interval);
+        if self.animation_timer.is_event(event).is_some() {
             self.start_time = Some(Instant::now());
+            self.duration_timer = cx.start_timeout(self.duration);
         }
 
-        if self.timer.is_event(event).is_some() {
-            self.view.close(cx);
+        if self.duration_timer.is_event(event).is_some() {
+            self.update_animation(cx);
         }
-        self.view.handle_event(cx, event, scope);
-        self.widget_match_event(cx, event, scope);
-    }
 
-    fn draw_walk(&mut self, cx: &mut Cx2d, scope: &mut Scope, walk: Walk) -> DrawStep {
-        if let Some(start_time) = self.start_time {
-            let elapsed = start_time.elapsed().as_secs_f64();
-            let progress = (elapsed / self.duration).min(1.0);
-            let progress_bar_height = 100.0 * (1.0 - progress);
+        if self.redraw_timer.is_event(event).is_some() {
+            if let Some(start_time) = self.start_time {
+                let elapsed = start_time.elapsed().as_secs_f64();
+                let progress = (elapsed / self.duration).min(1.0);
+                let progress_bar_height = 100.0 * progress;
+    
+                self.view(id!(progress_bar)).apply_over(
+                    cx,
+                    live! {
+                        height: (progress_bar_height)
+                    } 
+                );
+            }
+        };
 
-            self.view.view(id!(progress_bar)).apply_over(
-                cx,
-                live! {
-                    height: (progress_bar_height)
-                } 
-            );
-            if elapsed >= self.duration {
-                self.view.close(cx);
-                return DrawStep::done();
+        if self.animator_handle_event(cx, event).must_redraw() {
+            self.redraw(cx);
+        }
+
+        if let Event::MouseDown(e) = event {
+            if self.view(id!(close_icon)).area().rect(cx).contains(e.abs) {
+                self.close(cx);
+                return;
             }
         }
-        self.view.draw_walk(cx, scope, walk)
-    }
-}
 
-impl WidgetMatchEvent for RobrixPopupNotification {
-    fn handle_actions(&mut self, cx: &mut Cx, actions: &Actions, scope: &mut Scope) {
-        
+        self.content.handle_event(cx, event, scope);
+    }
+
+    fn draw_walk(&mut self, cx: &mut Cx2d, scope: &mut Scope, _walk: Walk) -> DrawStep {
+        self.draw_list.begin_overlay_reuse(cx);
+
+        cx.begin_pass_sized_turtle(self.layout);
+        self.draw_bg.begin(cx, self.walk, self.layout);
+
+        if self.opened {
+            let _ = self.content.draw_all(cx, scope);
+        }
+
+        self.draw_bg.end(cx);
+
+        cx.end_pass_sized_turtle();
+        self.draw_list.end(cx);
+
+        DrawStep::done()
     }
 }
 
 impl RobrixPopupNotification {
-    // pub fn set_time(&mut self, duration: f64) {
-    //     // self.timer = cx.start_timeout(duration);
-    //     self.duration = duration;
-    // }
-}
+    pub fn open(&mut self, cx: &mut Cx) {
+        self.opened = true;
+        self.animation_timer = cx.start_timeout(2.0);
+        self.redraw_timer = cx.start_interval(0.016);
+        self.animator_play(cx, id!(mode.open));
+        self.redraw(cx);
+    }
 
+    pub fn close(&mut self, cx: &mut Cx) {
+        cx.stop_timer(self.redraw_timer);
+        self.animator_play(cx, id!(mode.close));
+        self.redraw(cx);
+    }
+
+    pub fn update_animation(&mut self, cx: &mut Cx) {
+        if self.animator_in_state(cx, id!(mode.open)) {
+            cx.stop_timer(self.redraw_timer);
+            self.animator_play(cx, id!(mode.close));
+        }
+    }
+}
 
 impl RobrixPopupNotificationRef {
-    // pub fn set_time(&mut self, duration: f64) {
-    //     if let Some(mut inner) = self.borrow_mut() {
-    //         inner.set_time(duration);
-    //     }
-    // }
-}
+    pub fn open(&self, cx: &mut Cx) {
+        if let Some(mut inner) = self.borrow_mut() {
+            inner.open(cx);
+        }
+    }
 
-#[derive(Clone, Debug, DefaultNone)]
-pub enum PopupNotificationAction {
-    None,
-    Close,
-    Open
+    pub fn close(&self, cx: &mut Cx) {
+        if let Some(mut inner) = self.borrow_mut() {
+            inner.close(cx);
+        }
+    }
 }
