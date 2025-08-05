c9a8bf4fde08c31c6baa5020f8511aa192c17810
Kevin Boos <kevinaboos@gmail.com>
2025-08-05 13:08:59 -0700

    fix warning

--- src/app.rs
+++ src/app.rs
@@ -186,11 +186,11 @@ impl MatchEvent for App {
         self.update_login_visibility(cx);
 
         log!("App::Startup: starting matrix sdk loop");
-        let tokio_rt = crate::sliding_sync::start_matrix_tokio().unwrap();
+        let _tokio_rt = crate::sliding_sync::start_matrix_tokio().unwrap();
 
         #[cfg(feature = "tsp")] {
             log!("App::Startup: initializing TSP (Trust Spanning Protocol) module.");
-            crate::tsp::tsp_init(tokio_rt).unwrap();
+            crate::tsp::tsp_init(_tokio_rt).unwrap();
         }
     }
 
